af8fb81fa35d4307f36a23f262ecf750c4ee0968
Tim <feok123@mail.ru>
2018-04-04 17:29:24 +0500

    addsomechanges

--- src/CarRegister/carregistrationtask.java
+++ src/CarRegister/carregistrationtask.java
@@ -6,42 +6,49 @@ import java.util.*;
 
 public class carregistrationtask implements Task {
     public int[] runTask() {
-        Persons();
 
         return new int[0];
     }
 
 
+      public class Linkedlist {
+          public  void main(String[] arg) {
 
-        public void Persons() {
+              LinkedList<String> persons = new LinkedList<String>();
+              persons.add(0, "Mike, A102BC, Nissan ");
+              persons.add(1, "Alex, M283HE, BMW  ");
+              persons.add(2, "Jack, O987AA, LADA ");
+              persons.add(3, "Kate, T654EE, Jeep  ");
+              persons.clone();
 
-            LinkedList<String> persons = new LinkedList<String>();
-            persons.add(0, "Mike, A102BC, Nissan ");
-            persons.add(1, "Alex, M283HE, BMW  ");
-            persons.add(2, "Jack, O987AA, LADA ");
-            persons.add(3, "Kate, T654EE, Jeep  ");
 //        persons.remove(1);
-            System.out.println(persons);
-            Scanner sc = new Scanner(System.in);
-            String cm;
-            if(sc.hasNextInt()) {
-                cm = sc.nextLine(); //
-                System.out.println();
-            }
-//            Scanner sc = new Scanner(System.in);
-//            String cm;
-//            cm = sc.nextLine();
-//            do {
-//                persons.removeLast();
-//            }
-//            while (cm == persons.remove());
+              System.out.println(persons);
+//                  Scanner sc = new Scanner(System.in);
+//                  String cm;
+//                  if (sc.hasNextInt()) {
+//                      cm = sc.nextLine(); //
+//                      System.out.println();
+//                  }
+//                  Scanner sc = new Scanner(System.in);
+//                  String cm;
+//                  cm = sc.nextLine();
 
 
+          }
+      }
 
+//      public class Delete extends Linkedlist {
+//
+//      }
+//
+//      public class History extends Linkedlist{
+//
+//      }
+//
+//      public static void Split(){
+//
+//      }
 
-
-
-        }
 
     }
 
