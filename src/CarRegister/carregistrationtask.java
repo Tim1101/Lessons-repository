6f1103011be5ecb30b6e65cd20d9223c1c28e481
Tim <feok123@mail.ru>
2018-04-12 15:49:00 +0500

    addsomechanges

--- src/CarRegister/carregistrationtask.java
+++ src/CarRegister/carregistrationtask.java
@@ -6,48 +6,46 @@ import java.util.*;
 
 public class carregistrationtask implements Task {
     public int[] runTask() {
-
+        Person();
         return new int[0];
+
+    }
+
+    public class Linkedlist {
+        LinkedList<String> persons = new LinkedList<String>();
     }
 
 
-      public class Linkedlist {
-          public  void main(String[] arg) {
+    public class Person1 extends Linkedlist {
+    }
 
-              LinkedList<String> persons = new LinkedList<String>();
-              persons.add(0, "Mike, A102BC, Nissan ");
-              persons.add(1, "Alex, M283HE, BMW  ");
-              persons.add(2, "Jack, O987AA, LADA ");
-              persons.add(3, "Kate, T654EE, Jeep  ");
-              persons.clone();
+    public static void Person() {
 
-//        persons.remove(1);
-              System.out.println(persons);
-//                  Scanner sc = new Scanner(System.in);
-//                  String cm;
-//                  if (sc.hasNextInt()) {
-//                      cm = sc.nextLine(); //
-//                      System.out.println();
-//                  }
-//                  Scanner sc = new Scanner(System.in);
-//                  String cm;
-//                  cm = sc.nextLine();
+        persons.add(0, "Mike, A102BC, Nissan ");
+        persons.add(1, "Alex, M283HE, BMW  ");
+        persons.add(2, "Jack, O987AA, LADA ");
+        persons.add(3, "Kate, T654EE, Jeep  ");
+        persons.clone();
+//              persons.remove(1);
+        System.out.print(persons);
+
+    }
+
+}
+            public static void Delete(){
+
+            }
+//
 
 
-          }
-      }
 
-//      public class Delete extends Linkedlist {
-//
-//      }
-//
-//      public class History extends Linkedlist{
-//
-//      }
-//
-//      public static void Split(){
-//
-//      }
+
+
+        public static void Split(){
+
+        }
+
+
 
 
     }
