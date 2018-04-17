019e6b946d5b8f22846eca9396101ca0a83c7055
Tim <feok123@mail.ru>
2018-04-17 14:55:45 +0500

    corrections

--- src/CarRegister/carregistrationtask.java
+++ src/CarRegister/carregistrationtask.java
@@ -6,44 +6,45 @@ import java.util.*;
 
 public class carregistrationtask implements Task {
     public int[] runTask() {
+        Scanner sc = new Scanner(System.in);
+        String comand;
+        LinkedList();
         Person();
         return new int[0];
-
     }
 
-    public class Linkedlist {
+    public static void LinkedList() {
         LinkedList<String> persons = new LinkedList<String>();
-    }
-
-
-    public class Person1 extends Linkedlist {
+        for (int i = 0; i < persons.size(); i++){
+            persons.addLast(" ");
+        }
+        System.out.println(persons);
+        
     }
 
     public static void Person() {
-
+        LinkedList<String> persons = new LinkedList<String>();
+        Scanner sc = new Scanner(System.in);
+        boolean cmd;
+        System.out.println("Please enter the command");
         persons.add(0, "Mike, A102BC, Nissan ");
         persons.add(1, "Alex, M283HE, BMW  ");
         persons.add(2, "Jack, O987AA, LADA ");
         persons.add(3, "Kate, T654EE, Jeep  ");
-        persons.clone();
-//              persons.remove(1);
+//      persons.remove(1);
         System.out.print(persons);
 
     }
 
-}
-            public static void Delete(){
-
-            }
-//
 
 
 
 
 
-        public static void Split(){
 
-        }
+
+
+
 
 
 
