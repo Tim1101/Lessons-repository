f399fae474d23cf1119b42d90cf48b898e1b910e
Tim <feok123@mail.ru>
2018-05-17 15:58:53 +0500

    1.12

--- src/CarRegister/carregistrationtask.java
+++ src/CarRegister/carregistrationtask.java
@@ -12,29 +12,44 @@ public class carregistrationtask implements Task {
 
 
     public static void Person() {
-//        LinkedList<String> persons = new LinkedList<String>();
-//        Scanner sc = new Scanner(System.in);
+        LinkedList<String> persons = new LinkedList<String>();
+        Scanner sc = new Scanner(System.in);
 //        String delete = sc.nextLine();
 //        String add = sc.nextLine();
-//        System.out.println("Please enter the command");
-//        persons.add(0, "Mike, A102BC, Nissan ");
-//        persons.add(1, "Alex, M283HE, BMW  ");
-//        persons.add(2, "Jack, O987AA, LADA ");
-//        persons.add(3, "Kate, T654EE, Jeep  ");
-//        LinkedList<String> persons1 = new LinkedList<String>(persons.subList(0, 1));
-////      persons1.remove(0);
-//        System.out.println(persons1);
-//        LinkedList<String> persons2 = new LinkedList<String>(persons.subList(1, 2));
-//        System.out.println(persons2);
-//        LinkedList<String> persons3 = new LinkedList<String>(persons.subList(2, 3));
-//        System.out.println(persons3);
-//        LinkedList<String> persons4 = new LinkedList<String>(persons.subList(3, 4));
-//        System.out.println(persons4);
-//
-//        if (delete == sc.nextLine()){
-//            persons1.remove(0);
+        System.out.println("Please enter the command");
+        persons.add(0, "Mike, A102BC, Nissan ");
+        persons.add(1, "Alex, M283HE, BMW  ");
+        persons.add(2, "Jack, O987AA, LADA ");
+        persons.add(3, "Kate, T654EE, Jeep  ");
+
+
+
+
+//        for (int i = 0; i < persons.size(); i++){
+//            String element = sc.next();
+//            persons.add(element);
 //        }
+//        System.out.println(persons);
+
+        LinkedList<String> persons1 = new LinkedList<String>(persons.subList(0, 1));
+//      persons1.remove(0);
+        System.out.println(persons1);
+        LinkedList<String> persons2 = new LinkedList<String>(persons.subList(1, 2));
+        System.out.println(persons2);
+        LinkedList<String> persons3 = new LinkedList<String>(persons.subList(2, 3));
+        System.out.println(persons3);
+        LinkedList<String> persons4 = new LinkedList<String>(persons.subList(3, 4));
+        System.out.println(persons4);
+
+//        for (int i = 0; i < persons.size(); i ++){
+//            if (persons.size() == 4){
+//                System.out.println("Please enter the command");
 //
+//            }
+//        }
+
+
+
 //       if (add == sc.nextLine()){
 //            persons.add(4, "Bob, E832MM, Gelly");
 //            LinkedList<String> persons5 = new LinkedList<String>(persons.subList(4, 5));
@@ -42,20 +57,21 @@ public class carregistrationtask implements Task {
 //        }
 
 
-        LinkedList<String> list=new LinkedList<String>();
-        Scanner input=new Scanner(System.in);
-        System.out.print("how many elemenst do you want to add: ");
-        int num=input.nextInt();
-        for(int i=0;i<num;i++){
-            System.out.print("Add Element: ");
-            String element=input.next();
-            list.add(element);
-        }
-        System.out.println();
-        System.out.println("List elements are: ");
-        for(int i=0;i<list.size();i++){
-            System.out.println(list.get(i));
-        }
+//        LinkedList<String> list = new LinkedList<String>();
+//        Scanner input = new Scanner(System.in);
+//        Scanner ppl = new Scanner(System.in);
+//        System.out.print("how many elemenst do you want to add: ");
+//        int num = input.nextInt();
+//        for(int i = 0;i < num; i++){
+//            System.out.print("Add Element: ");
+//            String element=input.next();
+//            list.add(element);
+//        }
+//        System.out.println();
+//        System.out.println("List elements are: ");
+//        for(int i = 0;i < list.size(); i++){
+//            System.out.println(list.get(i));
+//        }
 
 
 
