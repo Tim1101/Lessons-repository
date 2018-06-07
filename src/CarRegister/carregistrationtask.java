aeb9e1c714d41af115d65f543ea3d7f31d50061b
Tim <feok123@mail.ru>
2018-06-07 15:50:56 +0500

    addtwofunc

--- src/CarRegister/carregistrationtask.java
+++ src/CarRegister/carregistrationtask.java
@@ -27,58 +27,64 @@ public class carregistrationtask implements Task {
 
 
 
-//        for (int i = 0; i < persons.size(); i++){
-//            String element = sc.next();
-//            persons.add(element);
-//        }
-//        System.out.println(persons);
-//        for ( int i = 0; i < persons.size(); i++ ){
-//            LinkedList<String> persons5 = new LinkedList<String>(persons.subList(4,5));
-//            System.out.println(persons5);
-//        }
+        for (int i = 0; i < persons.size(); i++){
+            String element = sc.next();
+            persons.add(element);
+        }
+        System.out.println(persons);
+        for ( int i = 0; i < persons.size(); i++ ){
 
-        LinkedList<String> persons1 = new LinkedList<String>(persons.subList(0, 1));
-//      persons1.remove(0);
-        System.out.println(persons1);
-        LinkedList<String> persons2 = new LinkedList<String>(persons.subList(1, 2));
-        System.out.println(persons2);
-        LinkedList<String> persons3 = new LinkedList<String>(persons.subList(2, 3));
-        System.out.println(persons3);
-        LinkedList<String> persons4 = new LinkedList<String>(persons.subList(3, 4));
-        System.out.println(persons4);
+        }
+
+//        LinkedList<String> persons1 = new LinkedList<String>(persons.subList(0, 1));
+////      persons1.remove(0);
+//        System.out.println(persons1);
+//        LinkedList<String> persons2 = new LinkedList<String>(persons.subList(1, 2));
+//        System.out.println(persons2);
+//        LinkedList<String> persons3 = new LinkedList<String>(persons.subList(2, 3));
+//        System.out.println(persons3);
+//        LinkedList<String> persons4 = new LinkedList<String>(persons.subList(3, 4));
+//        System.out.println(persons4);
+
+
+        LinkedList<String> lists = new LinkedList<String>();
+        Scanner move = new Scanner(System.in);
+        System.out.println("What do you want to do?");
+        String act = move.next();
+        for (int i = 0; i < lists.size(); i++){
+            String add = null;
+            String delete = null;
+            if (act == add) {
+                String element = move.next();
+                lists.add(element);
+            }
+            else {
+                String element = move.next();
+                lists.remove(element);
+            }
+        }
+        System.out.println();
+        System.out.println("List elements are");
+        for (int i = 0; i < lists.size(); i++){
+            System.out.println(lists.get(i));
+        }
 
 //
-//        for (int i = 0; i < persons.size(); i ++){
-//            if (persons.size() == 4){
-//                System.out.println("Please enter the command");
-//
-//            }
-//        }
-
-
-//
-//       if (add == sc.nextLine()){
-//            persons.add(4, "Bob, E832MM, Gelly");
-//            LinkedList<String> persons5 = new LinkedList<String>(persons.subList(4, 5));
-//            System.out.println(persons5);
-//        }
-
-//
-//        LinkedList<String> list = new LinkedList<String>();
-//        Scanner input = new Scanner(System.in);
-//        Scanner ppl = new Scanner(System.in);
-//        System.out.print("how many elemenst do you want to add: ");
-//        int num = input.nextInt();
-//        for(int i = 0;i < num; i++){
-//            System.out.print("Add Element: ");
-//            String element=input.next();
-//            list.add(element);
-//        }
-//        System.out.println();
-//        System.out.println("List elements are: ");
-//        for(int i = 0;i < list.size(); i++){
-//            System.out.println(list.get(i));
-//        }
+        LinkedList<String> list = new LinkedList<String>();
+        Scanner input = new Scanner(System.in);
+        Scanner ppl = new Scanner(System.in);
+        System.out.print("how many elemenst do you want to add: ");
+        int num = input.nextInt();
+        for(int i = 0;i < num; i++){
+            System.out.print("Add Element: ");
+            String element=input.next();
+            list.add(element);
+        }
+        System.out.println();
+        System.out.println("List elements are: ");
+        for(int i = 0;i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
 
 
 
